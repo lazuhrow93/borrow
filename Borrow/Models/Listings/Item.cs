386d7435903c5c55280115b7014a296a40afc0e7
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2023-04-20 20:40:19 -0500

    pushing new stuff

---
+++ Borrow/Models/Listings/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Borrow.Models.Listings
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Age { get; set; }
+    }
+}
--- Borrow/Models/Views/ItemViewModel.cs
+++ Borrow/Models/Views/ItemViewModel.cs
@@ -1,13 +1,11 @@
-﻿namespace Borrow.Models.Views
+﻿using Borrow.Models.Listings;
+
+namespace Borrow.Models.Views
 {
-    public class ItemViewModel
+    public class ItemViewModel : Item
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
         public decimal DailyRate { get; set; }
         public decimal WeeklyRate { get; set; }
         public bool Available { get; set; }
-        public int Age { get; set; }
     }
 }
