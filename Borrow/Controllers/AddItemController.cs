92251eeb79d1dff6a2dec84681b10ccf3103a7b1
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2023-04-27 00:58:55 -0500

    added the models and views. Need ot implement the navigation

---
+++ Borrow/Controllers/AddItemController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Borrow.Data.DataAccessLayer.Interfaces;
+using Borrow.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Borrow.Controllers
+{
+    [Authorize]
+    public class AddItemController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserDataAccess _uda;
+
+        public AddItemController(IMapper mapper, IUserDataAccess ia)
+        {
+            _mapper = mapper;
+            _uda = ia;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
