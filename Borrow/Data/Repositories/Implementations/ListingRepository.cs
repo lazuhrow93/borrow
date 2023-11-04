263aa6156c84e7c5e109b5786a8dfa50b53e8fa6
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2023-11-04 13:35:32 -0500

    added listing repo and service

---
+++ Borrow/Data/Repositories/Implementations/ListingRepository.cs
@@ -0,0 +1,42 @@
+﻿using Borrow.Data.Repositories.Interfaces;
+using Borrow.Models.Backend;
+using Microsoft.EntityFrameworkCore;
+
+namespace Borrow.Data.Repositories.Implementations
+{
+    public class ListingRepository : IRepository<Listing>
+    {
+        private DbContext Db { get; set; }
+        public IQueryable<Listing> Query => throw new NotImplementedException();
+
+        public void Add(Listing entity)
+        {
+            Db.Add(entity);
+        }
+
+        public void Add(List<Listing> entity)
+        {
+            Db.AddRange(entity);
+        }
+
+        public void Delete(Listing entity)
+        {
+            Db.Remove(entity);
+        }
+
+        public List<Listing> FetchAll()
+        {
+            return Query.ToList();
+        }
+
+        public Listing GetById(int id)
+        {
+            return Query.First(l => l.Id == id);
+        }
+
+        public void Save()
+        {
+            Db.SaveChanges();
+        }
+    }
+}
---
+++ Borrow/Data/Services/Listing/IListingService.cs
@@ -0,0 +1,18 @@
+﻿using Borrow.Models.Views;
+using Borrow.Models.Views.Listings;
+using System;
+
+namespace Borrow.Data.Services.Listing
+{
+    public interface IListingService
+    {
+        public UserListingsViewModel GetUserListingsViewModel();
+        public CreateListingViewModel GetCreateListingViewModel();
+        public PublishListingViewModel GetPublishListingViewModel();
+        public RemoveListingViewModel GetRemoveListingViewModel();
+        public ViewListingViewModel GetListingViewModel();
+        public NeighborhoodListingsViewModel GetNeighborhoodListingsViewModel();
+        public bool CreateListing();
+        public bool DeactiveListing(); 
+    }
+}
